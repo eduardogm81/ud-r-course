5b3c7f4f174352c1ca1775c9d69b5969e7b054fe
eduardogm81qg <eduardogm@gmail.com>
2018-04-18 14:58:54 +0200

    Tema 1 -> Read fwf

---
+++ scripts/tema1/04-analisis-fwf.R
@@ -0,0 +1,10 @@
+# fwf -> Fixed Width Format Files
+
+students_data <- read.fwf("../data/tema1/student-fwf.txt",
+                          widths = c(4, 15, 20, 15, 4),
+                          col.names = c("id", "nombre", "email", "carrera", "año"))
+
+
+students_data_header <- read.fwf("../data/tema1/student-fwf-header.txt",
+                                 widths = c(4, 15, 20, 15, 4), header = TRUE, 
+                                 sep = "\t", skip = 2)
